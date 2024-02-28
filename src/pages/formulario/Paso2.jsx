05920ccc762fa461a607d448a0c92ccfa23d9c58
Leonardo Cortes <leodistrital@hotmail.com>
2024-02-28 08:42:32 -0500

    se ajusto paso tres fotografia periodistica

--- src/pages/formulario/Paso2.jsx
+++ src/pages/formulario/Paso2.jsx
@@ -2,42 +2,49 @@ import { HeaderPaso } from "../../componentes/formulario/global/HeaderPaso";
 import { useAppStore } from "../../stores/app.store";
 import { useNavigate } from "react-router-dom";
 import { categoriasData } from "../../stores/categorias";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Paso2 = () => {
+	const [classfoto, setclassfoto] = useState("inactivar_subCat");
+
 	const navigate = useNavigate();
 
 	const setcategoria = useAppStore((state) => state.setcategoria);
 	const tipoTrabajo = useAppStore((state) => state.inscripcion.tipoTrabajo);
 
-  let datacategorias=[];
+	// const clasfoto ='leonardo';
 
-  if(tipoTrabajo===1){
-    datacategorias = categoriasData; 
-  }
-  if(tipoTrabajo===2){
-   datacategorias  =categoriasData.filter((item) => item.relacion_cat === `1`);
-  }
-  
+	let datacategorias = [];
 
-// console.log(datacategorias);
+	if (tipoTrabajo === 1) {
+		datacategorias = categoriasData;
+	}
+	if (tipoTrabajo === 2) {
+		datacategorias = categoriasData.filter(
+			(item) => item.relacion_cat === `1`
+		);
+	}
 
-  // console.log(data);
+	// console.log(datacategorias);
+
+	// console.log(data);
 	const setTrabajo = (data) => {
 		// console.log("llego", data);
 		setcategoria(data);
 		navigate("/paso3");
 	};
 
-  useEffect(() => {
-    if(tipoTrabajo===0){
-      console.log('error');
-      navigate("/panel");
-    }
+	const mostarcategoria = () => {
+		// console.log("mostrando");
+		setclassfoto("");
+	};
 
-
-  }, []);
-
+	useEffect(() => {
+		if (tipoTrabajo === 0) {
+			console.log("error");
+			navigate("/panel");
+		}
+	}, []);
 
 	return (
 		<>
@@ -62,17 +69,63 @@ export const Paso2 = () => {
 							<ul>
 								{datacategorias.map((item, index) => {
 									return (
-										<li key={index} className='itemCate '>
+										<li
+											key={index}
+											className={
+												item.cod_cat == "5" ||
+												item.cod_cat == "18"
+													? "itemCate  " + classfoto
+													: "itemCate "
+											}>
 											<div className='cLeft'>
-												<a
-													onClick={() =>
-														setTrabajo(item.cod_cat)
-													}
-													className='gBtn w100 txtUp btnStep '>
-													<span  dangerouslySetInnerHTML={{
-														__html: item.nom_cat,
-													}}/>
-												</a>
+												{item.cod_cat == "0" ? (
+													<a
+														onClick={() =>
+															mostarcategoria()
+														}
+														className={
+															item.cod_cat ==
+																"5" ||
+															item.cod_cat == "18"
+																? "gBtn w100 txtUp btnStep  sub_categoria" +
+																  "ocultar"
+																: "gBtn w100 txtUp btnStep "
+														}>
+														<span
+															dangerouslySetInnerHTML={{
+																__html:
+																	item.nom_cat,
+															}}
+														/>
+														<span>
+															{item.cod_cat}
+														</span>
+													</a>
+												) : (
+													<a
+														onClick={() =>
+															setTrabajo(
+																item.cod_cat
+															)
+														}
+														className={
+															item.cod_cat ==
+																"5" ||
+															item.cod_cat == "18"
+																? "gBtn w100 txtUp btnStep  sub_categoria"
+																: "gBtn w100 txtUp btnStep "
+														}>
+														<span
+															dangerouslySetInnerHTML={{
+																__html:
+																	item.nom_cat,
+															}}
+														/>
+														<span>
+															{item.cod_cat}
+														</span>
+													</a>
+												)}
 											</div>
 											<div className='cRight'>
 												{/* {item.desc_cat} */}
@@ -82,6 +135,10 @@ export const Paso2 = () => {
 													}}
 												/>
 											</div>
+
+											{item.cod_cat == "0" && (
+												<h1>leoxxxxxxxxxxxxxxxxxx</h1>
+											)}
 										</li>
 									);
 								})}
